b28ab801c28d34de59a9d8555c048fd14415dd15
hamza-akram-sct <hamzaakram.shyk172gmail.com>
2021-11-30 19:40:48 +0000

    Fix Login Issues ....

--- app/src/main/java/com/example/cleanarchitecture/data/repository/authmodule/AuthDataSource.kt
+++ app/src/main/java/com/example/cleanarchitecture/data/repository/authmodule/AuthDataSource.kt
@@ -3,5 +3,5 @@ package com.example.cleanarchitecture.data.repository.authmodule
 import com.example.cleanarchitecture.util.Resource
 
 interface AuthDataSource {
-    suspend fun login(email: String, password: String): Resource<String>
+    suspend fun login(email: String, password: String, coupan: Int): Resource<String>
 }
--- app/src/main/java/com/example/cleanarchitecture/data/repository/authmodule/AuthDataSourceImp.kt
+++ app/src/main/java/com/example/cleanarchitecture/data/repository/authmodule/AuthDataSourceImp.kt
@@ -3,7 +3,8 @@ package com.example.cleanarchitecture.data.repository.authmodule
 import com.example.cleanarchitecture.util.Resource
 
 class AuthDataSourceImp : AuthDataSource {
-    override suspend fun login(email: String, password: String): Resource<String> {
-        return Resource.Success(password)
+
+    override suspend fun login(email: String, password: String, coupan: Int): Resource<String> {
+        return Resource.Success("$password $coupan")
     }
 }
